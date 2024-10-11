d423a84be9cbea3e9b71c757d42bbfb60908b3db
jellyM0on <ylana.ong@gmail.com>
2024-10-11 20:16:17 +0800

    fix(client): fix redirection

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { addNotations } from './components/addNotations';
 import { addProjects } from './components/addProjects';
 
-//handle redirection
-if (window.location.hostname !== 'ylana-ong.com') {
-    window.location.href = 'ylana-ong.com' + window.location.pathname;
-}
-
 addProjects(); 
 addNotations();
 
