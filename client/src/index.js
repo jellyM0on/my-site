65d2b69600ddd73c4e012c76c2b3f5820eae9f80
jellyM0on <ylana.ong@gmail.com>
2024-10-11 18:17:42 +0800

    feat(client): add meta properties, favicon, and redirection

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { addNotations } from './components/addNotations';
 import { addProjects } from './components/addProjects';
 
+//handle redirection
+if (window.location.hostname !== 'ylana-ong.com') {
+    window.location.href = 'ylana-ong.com' + window.location.pathname;
+}
 
 addProjects(); 
 addNotations();
