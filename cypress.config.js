77d970e8c33576d6ab69360382983fbc6b77caf4
jellyM0on <ylana.ong@gmail.com>
2024-10-22 00:02:25 +0800

    test: update baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080/',
+    baseUrl: process.env.BASE_URL || 'http://localhost:8080/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
